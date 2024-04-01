f88b291cad872077dfd861a11d1540d8355cf3b7
rubicus3 <laba625@yandex.ru>
2024-04-01 16:39:46 +0300

    Camrea rotation

--- glView.cpp
+++ glView.cpp
@@ -19,11 +19,25 @@ void glView::initializeGL()
 void glView::resizeGL(int w, int h)
 {
     glViewport(0, 0, w, h);
+//    glMatrixMode(GL_PROJECTION);
+
 }
 
 void glView::paintGL()
 {
     glClearColor(0.25, 0.1, 0.5, 0);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef(90, 0, 0, 1);
+
+    glBegin(GL_TRIANGLES);
+    glColor3d(1, 0, 0);
+    glVertex2d(0, 0);
+    glVertex2d(1, 0);
+    glVertex2d(0, 1);
+
+    glEnd();
 }
 
