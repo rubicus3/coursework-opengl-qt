502971dae0832e35c49d1f09daa411a545b6743d
rubicus3 <laba625@yandex.ru>
2024-04-01 16:43:06 +0300

    Perspective projection

--- glView.cpp
+++ glView.cpp
@@ -19,8 +19,10 @@ void glView::initializeGL()
 void glView::resizeGL(int w, int h)
 {
     glViewport(0, 0, w, h);
-//    glMatrixMode(GL_PROJECTION);
-
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+//    glOrtho(-1, 1, -1, 1, -1, 2);
+    glFrustum(-1, 1, -1, 1, 1, 2);
 }
 
 void glView::paintGL()
@@ -34,9 +36,9 @@ void glView::paintGL()
 
     glBegin(GL_TRIANGLES);
     glColor3d(1, 0, 0);
-    glVertex2d(0, 0);
-    glVertex2d(1, 0);
-    glVertex2d(0, 1);
+    glVertex3d(0, 0, -1.5);
+    glVertex3d(1, 0, -1.5);
+    glVertex3d(0, 1, -1.5);
 
     glEnd();
 }
