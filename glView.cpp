eec399ca358ffd66e5322905561a07070826dd09
rubicus3 <laba625@yandex.ru>
2024-04-01 16:50:19 +0300

    Use vertex buffer

--- glView.cpp
+++ glView.cpp
@@ -25,6 +25,8 @@ void glView::resizeGL(int w, int h)
     glFrustum(-1, 1, -1, 1, 1, 2);
 }
 
+float arr[] = {0, 0, -1.5, 1, 0, -1.5, 0, 1, -1.5};
+
 void glView::paintGL()
 {
     glClearColor(0.25, 0.1, 0.5, 0);
@@ -34,12 +36,20 @@ void glView::paintGL()
     glLoadIdentity();
     glRotatef(90, 0, 0, 1);
 
-    glBegin(GL_TRIANGLES);
-    glColor3d(1, 0, 0);
-    glVertex3d(0, 0, -1.5);
-    glVertex3d(1, 0, -1.5);
-    glVertex3d(0, 1, -1.5);
 
-    glEnd();
+    glVertexPointer(3, GL_FLOAT, 0, &arr);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glColor3d(1, 0, 0);
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+//    glBegin(GL_TRIANGLES);
+//    glColor3d(1, 0, 0);
+//    glVertex3d(0, 0, -1.5);
+//    glVertex3d(1, 0, -1.5);
+//    glVertex3d(0, 1, -1.5);
+//    glEnd();
+
 }
 
