36e433a8e05fd80d2983aa8d4134a60dcf3fecf6
rubicus3 <laba625@yandex.ru>
2024-04-14 00:43:18 +0300

    Draw cube

--- glView.cpp
+++ glView.cpp
@@ -21,35 +21,79 @@ void glView::resizeGL(int w, int h)
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-//    glOrtho(-1, 1, -1, 1, -1, 2);
-    glFrustum(-1, 1, -1, 1, 1, 2);
+    glFrustum(-1, 1, -1, 1, 1, 3);
 }
-
-float arr[] = {0, 0, -1.5, 1, 0, -1.5, 0, 1, -1.5};
 
 void glView::paintGL()
 {
+    glEnable(GL_DEPTH_TEST);
     glClearColor(0.25, 0.1, 0.5, 0);
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glRotatef(90, 0, 0, 1);
+
+    glTranslatef(0, 0, -2);
+
+    glRotatef(xRot, 1, 0, 0);
+    glRotatef(yRot, 0, 1, 0);
+
+    drawCube(0.5);
+}
+
+void glView::mousePressEvent(QMouseEvent* mo) {
+    mPos = mo->pos();
+}
+
+void glView::mouseMoveEvent(QMouseEvent* mo) {
+    xRot = 1 / M_PI * (mo->pos().y() - mPos.y());
+    yRot = 1 / M_PI * (mo->pos().x() - mPos.x());
+    update();
+}
 
 
-    glVertexPointer(3, GL_FLOAT, 0, &arr);
+
+
+void glView::drawCube(float a)
+{
+
+    float ver_cub[] = {
+        -a, -a,  a,   a, -a,  a,    a,  a,  a,    -a,  a,  a,
+         a, -a, -a,  -a, -a, -a,   -a,  a, -a,     a,  a, -a,
+        -a, -a, -a,  -a, -a,  a,   -a,  a,  a,    -a,  a, -a,
+         a, -a,  a,   a, -a, -a,    a,  a,- a,     a,  a,  a,
+        -a, -a,  a,   a, -a,  a,    a, -a, -a,    -a, -a, -a,
+        -a,  a,  a,   a,  a,  a,    a,  a, -a,    -a,  a, -a,
+    };
+
+    float color_arr[] = {
+        1, 0, 0,   1, 0, 0,   1, 0, 0,   1, 0, 0,
+        0, 0, 1,   0, 0, 1,   0, 0, 1,   0, 0, 1,
+        1, 1, 0,   1, 1, 0,   1, 1, 0,   1, 1, 0,
+        0, 1, 1,   0, 1, 1,   0, 1, 1,   0, 1, 1,
+        1, 0, 1,   1, 0, 1,   1, 0, 1,   1, 0, 1,
+        1, 0.5, 0.5, 1, 0.5, 0.5, 1, 0.5, 0.5, 1, 0.5, 0.5,
+    };
+
+    glVertexPointer(3, GL_FLOAT, 0, &ver_cub);
+    glColorPointer(3, GL_FLOAT, 0, &color_arr);
 
     glEnableClientState(GL_VERTEX_ARRAY);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-    glColor3d(1, 0, 0);
+    glEnableClientState(GL_COLOR_ARRAY);
+
+    glDrawArrays(GL_QUADS, 0, 24);
+
     glDisableClientState(GL_VERTEX_ARRAY);
-
-//    glBegin(GL_TRIANGLES);
-//    glColor3d(1, 0, 0);
-//    glVertex3d(0, 0, -1.5);
-//    glVertex3d(1, 0, -1.5);
-//    glVertex3d(0, 1, -1.5);
-//    glEnd();
-
+    glDisableClientState(GL_COLOR_ARRAY);
 }
+
+
+
+
+
+
+
+
+
+
+
 
--- glView.h
+++ glView.h
@@ -1,17 +1,25 @@
 #ifndef GLVIEW_H
 #define GLVIEW_H
 #include <QOpenGLWidget>
+#include <QMouseEvent>
+#include <QtMath>
 
 class glView : public QOpenGLWidget
 {
     Q_OBJECT
+private:
+    float xRot, yRot, zRot;
+    QPoint mPos;
 
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+
+    void mousePressEvent(QMouseEvent*) override;
+    void mouseMoveEvent(QMouseEvent*) override;
 public:
     glView(QWidget *parent = nullptr);
     ~glView();
-    void initializeGL();
-    void resizeGL(int w, int h);
-    void paintGL();
-
+    void drawCube(float);
 };
 #endif // GLVIEW_H
